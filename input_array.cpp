d8b6632666561e5ea394e4b4c9a737463a2901dc
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-10-31 00:07:36 -0700

    added some code

--- input_array.cpp
+++ input_array.cpp
@@ -1,3 +1,28 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+using namespace std;
 
-// TODO: Your code
+extern "C" signed long input_array(signed long* ptr, signed long size){
+	signed long input = 0;
+	signed long count = 0;
+	
+	//step 3
+	cout << "Input your integer data one line at a time and enter 'q' when finished.\n";
+	
+	while(true){
+		cout << "Enter the next integer: ";
+		cin >> input;
+		if(cin.fail()){
+			cout << "You entered nonsense! Assuming you are done.";
+			return count;
+			}
+		else if(input){
+			//append the user input to the array
+			return 0;
+			}
+		*(ptr+count) = input;
+		count++;
+	}
 
+}

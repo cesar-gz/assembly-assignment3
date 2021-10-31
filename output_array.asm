2344a9a98df2bac6caee9144ae6a693f3c6248cb
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-10-31 15:52:14 -0700

    added code

--- output_array.asm
+++ output_array.asm
@@ -6,19 +6,39 @@ FD_STDOUT					equ		1
 CRLF						db		13,10
 CRLF_LEN					equ		$-CRLF
 
-WELCOME					db	"Test."
-WELCOME_LEN		equ	$-WELCOME
+A					db	"( "
+A_LEN		equ	$-A
+B					db	" integers)"
+B_LEN		equ	$-B
+C					db	", "
+C_LEN		equ	$-C
 
 section .text
+
+extern libPuhfessorP_printSignedInteger64
 
 global output_array
 output_array:
 
+	push r14
+	push r15
+	mov r14, rdi
+	mov r15, rsi
+
 	mov rax, SYS_WRITE
 	mov rdi, FD_STDOUT
-	mov rsi, WELCOME
-	mov rdx, WELCOME_LEN
+	mov rsi, A
+	mov rdx, A_LEN
 	syscall
+	
+	mov rdi, r15
+	call libPuhfessorP_printSignedInteger64
+	call crlf
+	
+	pop r15
+	pop r14
+	mov rax, 0
+	ret
 
 crlf:
 	

061ce892c48362d906caccf30880ec98efdca43b
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-10-31 00:50:41 -0700

    added code

--- output_array.asm
+++ output_array.asm
@@ -1,3 +1,31 @@
+section .data
 
-; TODO: Your code
+SYS_WRITE					equ		1
+FD_STDOUT					equ		1
 
+CRLF						db		13,10
+CRLF_LEN					equ		$-CRLF
+
+WELCOME					db	"Test."
+WELCOME_LEN		equ	$-WELCOME
+
+section .text
+
+global output_array
+output_array:
+
+	mov rax, SYS_WRITE
+	mov rdi, FD_STDOUT
+	mov rsi, WELCOME
+	mov rdx, WELCOME_LEN
+	syscall
+
+crlf:
+	
+	mov rax, SYS_WRITE
+	mov rdi, FD_STDOUT
+	mov rsi, CRLF
+	mov rdx, CRLF_LEN
+	syscall
+	
+	ret

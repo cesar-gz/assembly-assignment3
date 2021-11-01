ff75d49b915d7843e263deae21663b947b521405
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-10-31 17:32:50 -0700

    starting find largest

--- find_largest.asm
+++ find_largest.asm
@@ -1,3 +1,40 @@
+; CPSC 240, Cesar Gutierrez
 
-; TODO: Your code
+; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+section .data
+
+	SYS_WRITE					equ		1
+	FD_STDOUT					equ		1
+
+	CRLF						db		13,10
+	CRLF_LEN					equ		$-CRLF
+
+	M			db	"We are inside find_largest()"
+	M_LEN		equ	$-M
+
+section .text
+
+global find_largest
+find_largest:
+
+	mov rax, SYS_WRITE
+	mov rdi, FD_STDOUT
+	mov rsi, M
+	mov rdx, M_LEN
+	syscall
+	call crlf
+
+	mov rax, 0
+	ret
+
+crlf:
+	
+	mov rax, SYS_WRITE
+	mov rdi, FD_STDOUT
+	mov rsi, CRLF
+	mov rdx, CRLF_LEN
+	syscall
+	
+	ret
 

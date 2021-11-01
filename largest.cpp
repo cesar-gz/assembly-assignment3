2afe25525fec6b2a2b89d47a610b8d362bd5995b
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-10-31 22:37:17 -0700

    added returnValue

--- largest.cpp
+++ largest.cpp
@@ -11,10 +11,11 @@ extern "C" long manager();
 int main(){
 	
 	//step 1
+	long returnValue;
 	cout << endl;
 	cout << "Welcome to CPSC 240 Assignment 3 brought to you by Cesar Gutierrez." << endl;
-	manager();
-	cout << "The driver received this value: " <<endl;
+	returnValue = manager();
+	cout << "The driver received this value: "<< returnValue <<endl;
 	cout << "Have a nice day. The program will return control to the operating system." <<endl;
 	return 0;
 }

16eaa189b16b0bbd0844282231a13ffbb4f24c4a
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-10-31 17:47:30 -0700

    added extra cout lines

--- largest.cpp
+++ largest.cpp
@@ -10,6 +10,7 @@ int main(){
 	cout << endl;
 	cout << "Welcome to CPSC 240 Assignment 3 brought to you by Cesar Gutierrez." << endl;
 	manager();
+	cout << "The driver received this value: " <<endl;
 	cout << "Have a nice day. The program will return control to the operating system." <<endl;
 	return 0;
 }

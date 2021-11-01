9f736a62785ff7ad8e2f3307bbba635b0059edd9
Cesar Gutierrez <cesarg7@csu.fullerton.edu>
2021-10-31 17:47:01 -0700

    added steps 9 and 10

--- manager.asm
+++ manager.asm
@@ -15,6 +15,12 @@ section .data
 	MSG_LEN		equ	$-MSG
 	REC			db	"The following integers were received:"
 	REC_LEN		equ	$-REC
+	X			db	"The largest value "
+	X_LEN		equ	$-X
+	Y			db	" has been found at index "
+	Y_LEN		equ	$-Y
+	Z			db	"The manager will now return the count to the driver."
+	Z_LEN		equ	$-Z
 
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 section .text
@@ -22,6 +28,7 @@ section .text
 extern input_array
 extern output_array
 extern find_largest
+extern libPuhfessorP_printSignedInteger64
 
 global manager
 manager:
@@ -64,6 +71,30 @@ manager:
 	mov rdi, r12 ;array index
 	mov rsi, r10 ;count
 	call find_largest
+	
+	; step 9 & 10
+	mov rax, SYS_WRITE
+	mov rdi, FD_STDOUT
+	mov rsi, X
+	mov rdx, X_LEN
+	syscall
+	mov rdi, [r12] ;got this concept right just havn't return the right thing
+	call libPuhfessorP_printSignedInteger64
+	mov rax, SYS_WRITE
+	mov rdi, FD_STDOUT
+	mov rsi, Y
+	mov rdx, Y_LEN
+	syscall
+	mov rdi, r12 ;this is printing out the address not the index that im supposed to be printing
+	call libPuhfessorP_printSignedInteger64
+	call crlf
+	mov rax, SYS_WRITE
+	mov rdi, FD_STDOUT
+	mov rsi, Z
+	mov rdx, Z_LEN
+	syscall
+	call crlf
+	
 	
 	; clean up
 	mov rsp, rbp
